0ef31a0526ff32e63adc0052007dfac3e8ad37ba
emirtascakir <emirtascakir@icloud.com>
2024-03-26 08:45:53 +0300

    carousel slider added to home screen

--- e_commerce_mobile_app/lib/view/home/home_screen.dart
+++ e_commerce_mobile_app/lib/view/home/home_screen.dart
@@ -1,14 +1,26 @@
 import 'package:e_commerce_mobile_app/component/main_header.dart';
+import 'package:e_commerce_mobile_app/controller/controllers.dart';
+import 'package:e_commerce_mobile_app/view/home/components/carousel_slider/carousel_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Column(
-        children: [MainHeader()],
+        children: [
+          const MainHeader(),
+          Obx(() {
+            if (homeController.isBannerLoading.value) {
+              return const CarouselLoading();
+            } else {
+              return const CarouselLoading();
+            }
+          })
+        ],
       ),
     );
   }
